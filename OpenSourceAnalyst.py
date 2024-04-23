003fcb722a193754bae1e882364e8c8f2fabe7ea
PP-Mclappins <nathanwalker155@gmail.com>
2024-04-23 02:30:22 -0400

    Refined script, review realtime data on web.

--- OpenSourceAnalyst.py
+++ OpenSourceAnalyst.py
@@ -1,11 +1,13 @@
 import os
 from crewai import Agent, Task, Crew, Process
 from crewai_tools import SerperDevTool
+from crewai_tools import ScrapeWebsiteTool
+
 
 os.environ["OPENAI_API_BASE"] = 'http://localhost:11434/v1'
-os.environ["OPENAI_MODEL_NAME"]='mistral'
+os.environ["OPENAI_MODEL_NAME"] ='mistral'
 os.environ["OPENAI_API_KEY"] ='NA'
-os.environ["SERPER_API_KEY"] ="Insert API Key"
+os.environ["SERPER_API_KEY"]="Insert API Key"
 # You can choose to use a local model through Ollama for example. See https://docs.crewai.com/how-to/LLM-Connections/ for more information.
 
 # os.environ["OPENAI_API_BASE"] = 'http://localhost:11434/v1'
@@ -13,7 +15,7 @@ os.environ["SERPER_API_KEY"] ="Insert API Key"
 # os.environ["OPENAI_API_KEY"] =''
 
 search_tool = SerperDevTool()
-
+scrape_tool = ScrapeWebsiteTool(website_url='https://www.synopsys.com/blogs/software-security/open-source-trends-ossra-report.html')
 # Define your agents with roles and goals
 researcher = Agent(
   role='Opensource software enthusiast',
@@ -22,33 +24,28 @@ researcher = Agent(
   You have a knack for identifying new security threats emerging in the open source landscape and finding out how they work and what they do""",
   verbose=True,
   allow_delegation=True,
-  tools=[search_tool]
-  # You can pass an optional llm attribute specifying what mode you wanna use.
-  # It can be a local model through Ollama / LM Studio or a remote
-  # model like OpenAI, Mistral, Antrophic or others (https://docs.crewai.com/how-to/LLM-Connections/)
-  #
-  # import os
-  # os.environ['OPENAI_MODEL_NAME'] = 'gpt-3.5-turbo'
-  #
-  # OR
-  #
-  # from langchain_openai import ChatOpenAI
-  # llm=ChatOpenAI(model_name="gpt-3.5", temperature=0.7)
+  tools=[search_tool, scrape_tool],
+  max_rpm=100
+  
 )
 writer = Agent(
+    
   role='Opensource technology writer',
   goal='Craft understandable and direct literature on open source software security',
   backstory="""You work at a leading cyber security firm whose goal is to make opensource software safer""",
   verbose=True,
-  allow_delegation=False
+  allow_delegation=True,
+  tools=[search_tool, scrape_tool]
 )
 
 # Create tasks for your agents
 task1 = Task(
-  description="""Conduct a comprehensive analysis of the latest trends in opensource software vulnerabilities in 2024.
+  description="""Conduct a comprehensive search and analysis of the latest trends in opensource software vulnerabilities in 2024.
   Identify key trends, breakthrough technologies, potential industry impacts, and exploits being leveraged in the wild""",
-  expected_output="Fully drafted analysis",
-  agent=researcher
+  expected_output="Fully analysis with links to the information",
+  agent=researcher,
+  tools=[search_tool, scrape_tool],
+  human_input=True
 )
 
 task2 = Task(
@@ -56,7 +53,10 @@ task2 = Task(
   The article should be informative yet accessible, catering to a tech-savvy audience.
   Make it sound cool, Make sure that it's detailed and understandable""",
   expected_output="Full report in long article form",
-  agent=writer
+  agent=writer,
+  tools=[search_tool, scrape_tool],
+  context=[task1],
+  human_input=True
 )
 
 # Instantiate your crew with a sequential process
